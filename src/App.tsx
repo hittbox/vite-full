edd731a95f3fb0e0be8503d6913ff92941deb1a5
sophia22001 <yujii22001@gmail.com>
2025-11-26 17:40:16 +0900

    feat: 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
   return (
     <>
       <div className="text-blue-500 text-2xl">App Component</div>
+      <div className="text-red-500 text-3xl">tailwind css</div>
     </>
   );
 }
