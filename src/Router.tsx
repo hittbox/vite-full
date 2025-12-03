159417047b37a07d35e56aa0149f0ab2c5963089
jinj00oo <icems0428@naver.com>
2025-12-03 16:36:02 +0900

    chore: 라우팅 페이지 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import { ZustandPage } from './templates/ZustandTemplate';
 
 const Router = () => {
   return (
@@ -8,6 +9,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/zustand" element={<ZustandPage />} />
       </Routes>
     </BrowserRouter>
   );
