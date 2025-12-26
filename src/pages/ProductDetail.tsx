01340e820cd0639a590897d20a66fa464711233e
Yujin Kim <yujii22001@gmail.com>
2025-12-26 16:17:48 +0900

    feat: 제품 삭제 UI 추가

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,4 +1,4 @@
-import GrayButton from '@/shared/components/GrayButton';
+import CRUDButton from '@/shared/components/GrayButton';
 import { useNavigate, useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
@@ -8,16 +8,27 @@ const ProductDetail = () => {
   const name = `(서버에서 가져올) 상품 이름`;
   const price = 10000;
   const description = '상품 상세 설명입니다.';
+
+  const handleDeleteProduct = () => {
+    alert('정말로 삭제하시겠습니까?');
+    // 삭제 로직 구현
+    nav('/');
+  };
   return (
     <div className="m-20 flex flex-col gap-10">
       <div className="text-2xl">{name}</div>
-      <div className="flex justify-end">
-        <GrayButton
+      <div className="flex justify-end gap-2">
+        <CRUDButton
           onClick={() => {
             nav(`/shop/modify/${id}`);
           }}
           text="수정하기"
-        ></GrayButton>
+        ></CRUDButton>
+        <CRUDButton
+          className="bg-red-300"
+          onClick={handleDeleteProduct}
+          text="삭제하기"
+        ></CRUDButton>
       </div>
 
       <form className="flex flex-col gap-5 bg-gray-100">
--- src/shared/components/GrayButton.tsx
+++ src/shared/components/GrayButton.tsx
@@ -1,14 +1,20 @@
-interface GrayButtonProps {
+import { twMerge } from 'tailwind-merge';
+
+interface CRUDButtonProps {
   onClick: () => void;
   text: string;
+  className?: string;
 }
 
-const GrayButton = ({ onClick, text }: GrayButtonProps) => {
+const CRUDButton = ({ onClick, text, className }: CRUDButtonProps) => {
   return (
-    <button onClick={onClick} className="cursor-pointer rounded-2xl bg-gray-300 p-2">
+    <button
+      onClick={onClick}
+      className={twMerge(`cursor-pointer rounded-2xl bg-gray-300 p-2 ${className}`)}
+    >
       {text}
     </button>
   );
 };
 
-export default GrayButton;
+export default CRUDButton;
