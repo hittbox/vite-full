55a43becc2037d9b8bf768e5ffd29159f7b7c6dd
Emithen <86219540+Emithen@users.noreply.github.com>
2026-01-15 02:54:05 +0900

    feat: POST /product api 연동

--- src/remote/api/ProductApi.ts
+++ src/remote/api/ProductApi.ts
@@ -2,6 +2,7 @@ import { api } from "@/remote/axiosInstance";
 import type { GetProductResponse } from "../response/GetProductResponse";
 import type { ProductDomain } from "@/domain/Product";
 import { toProductDomain } from "../mapper/ProductMapper";
+import type { CreateProductRequest } from "../request/CreateProductRequest";
 
 export const getProductList = async (): Promise<ProductDomain[]> => {
   const res = await api.get<GetProductResponse[]>('/product/all');
@@ -11,4 +12,9 @@ export const getProductList = async (): Promise<ProductDomain[]> => {
 export const getProduct = async(id: string): Promise<ProductDomain> => {
   const res = await api.get<GetProductResponse>(`/product/${id}`);
   return toProductDomain(res.data);
+}
+
+export const createProduct = async(data: CreateProductRequest) => {
+  const res = await api.post('/product', data);
+  return res.data;
 }
---
+++ src/remote/request/CreateProductRequest.ts
@@ -0,0 +1,7 @@
+export type CreateProductRequest = {
+  name: string;
+  price: number;
+  description: string;
+  image_url: string;
+  created_by: string | null;
+}
